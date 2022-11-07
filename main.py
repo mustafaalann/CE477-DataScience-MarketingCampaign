86e72145d92e2c73667170e2635626c63577fc46
Wayese <115832840+Wayese@users.noreply.github.com>
2022-11-07 20:42:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import numpy
 import matplotlib.pyplot
-import pandas
+import pandas as pd
 import sklearn
 import scikit
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+path="https://www.kaggle.com/datasets/rodsaldanha/arketing-campaign?select=marketing_campaign.csv"
+df=pd.read_csv(path,header=none)
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+   print_hi('PyCharm')
 
