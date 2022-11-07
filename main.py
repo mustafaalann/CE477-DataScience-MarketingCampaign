b45cf31e336b203f76a80065ae840b0a990e8170
mustafaalann <mustafa.alann@hotmail.com>
2022-11-07 20:40:28 +0300

    Reading file from csv

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import numpy
-import matplotlib.pyplot
-import pandas
-import sklearn
-import scikit
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pandas as pd
 
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
+df = pd.read_csv('marketing_campaign.csv')
 
