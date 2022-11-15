cfd8d348d3adf87b1476c602eb77f796f48e3a3e
Wayese <115832840+Wayese@users.noreply.github.com>
2022-11-15 22:39:17 +0300

    full code of histogram-n-att

--- histogram-n-att.py
+++ histogram-n-att.py
@@ -4,12 +4,8 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("marketing_campaign.csv",delimiter=';')
 print(df.head(2240))
 print(df.columns)
-df[['Income', 'Kidhome',
-       'Teenhome', 'Dt_Customer', 'Recency', 'MntWines', 'MntFruits',
-       'MntMeatProducts', 'MntFishProducts', 'MntSweetProducts',
-       'MntGoldProds', 'NumDealsPurchases', 'NumWebPurchases',
-       'NumCatalogPurchases', 'NumStorePurchases', 'NumWebVisitsMonth',
-       'AcceptedCmp3', 'AcceptedCmp4', 'AcceptedCmp5', 'AcceptedCmp1',
-       'AcceptedCmp2', 'Complain', 'Z_CostContact', 'Z_Revenue', 'Response']].hist(bins=10, figsize=(8,6),alpha=0.4)
-plt.show()
+df[['Education']].value_counts().plot(kind='bar',figsize=(8,6),alpha=0.4)
 
+plt.show()
+df[['Marital_Status']].value_counts().plot(kind='bar',figsize=(8,6),alpha=0.4)
+plt.show()
