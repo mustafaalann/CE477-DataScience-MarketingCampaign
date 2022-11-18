cabc79a2417893d330578cdd431f0222681bf84e
Wayese <115832840+Wayese@users.noreply.github.com>
2022-11-18 21:10:14 +0300

    reviewed code of histogram-n-att

--- PCA.py
+++ PCA.py
@@ -1,18 +1,36 @@
+
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plot
+from sklearn.preprocessing import Normalizer
 
-df = pd.read_csv("marketing_campaign.csv",delimiter=';')
-columns_names = df.columns.tolist()
-print("Columns names:")
-print(columns_names)
-df.shape()
-df.head()
-df.corr()
-corr = df.corr()
-plt.figure(figsize=(10, 10))
-sns.heatmap(correlation, vmax=1, square=True, annot=True, cmap='cubehelix')
+df = pd.read_csv("marketing_campaign.csv", delimiter=';')
+columnsToRescale = df[['Income', 'Kidhome', 'Teenhome', 'Recency', 'MntWines', 'MntFruits',
+                      'MntMeatProducts', 'MntFishProducts', 'MntSweetProducts', 'MntGoldProds', 'NumDealsPurchases',
+                      'NumWebPurchases', 'NumCatalogPurchases', 'NumStorePurchases', 'NumWebVisitsMonth']].dropna()
+print('Before Normalization')
+print(columnsToRescale)
 
-plt.title('Correlation between different features')
+#Normalization
+scaler = Normalizer().fit(columnsToRescale)
+normalizedData = scaler.transform(columnsToRescale)
+normalizedData = pd.df(normalizedData,index=columnsToRescale.index,columns=columnsToRescale.columns)
+print('After Normalization')
+print(normalizedData)
+
+# Reformating and viewing results
+loadings = pd.df(pca.components_.T,
+columns=['PC%s' % _ for _ in range(len(df_normalizedData.columns))],
+index=df.columns)
+print(loadings)
+
+plot.plot(pca.explained_variance_ratio_)
+plot.ylabel('Explained Variance')
+plot.xlabel('Components')
+plot.show()
--- histogram-n-att.py
+++ histogram-n-att.py
@@ -4,13 +4,14 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("marketing_campaign.csv",delimiter=';')
 print(df.head(2240))
 print(df.columns)
+#drawing the histogram of nominal/Education attribute
 df[['Education']].value_counts().plot(kind='bar',figsize=(8,6),fontsize=(10),alpha=0.4)
 plt.xticks(rotation='horizontal')
 plt.title('Education')
 plt.xlabel('Education')
 plt.ylabel('Frequency')
 plt.show()
-
+#drawing the histogram of nominal/Marital Status attribute
 df[['Marital_Status']].value_counts().plot(kind='bar',figsize=(8,6),fontsize=(10),alpha=0.4)
 plt.title('Marital Status')
 plt.xticks(rotation='horizontal')
