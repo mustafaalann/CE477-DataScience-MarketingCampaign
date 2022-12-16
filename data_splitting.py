6efcc862c70381f562f674fc66784284c78d960f
Wayese <115832840+Wayese@users.noreply.github.com>
2022-12-16 23:14:16 +0300

    data splitting into train and test_update(5)

--- data_splitting.py
+++ data_splitting.py
@@ -10,6 +10,6 @@ y = df.iloc[:, -1] #the last column of data frame(Z_Revenue)
 
 # splitting the dataset into train and test
 x_train, x_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.33, random_state=0)
+    x, y, test_size=0.80, random_state=0)
 print(x_train)
 print(x_test)
