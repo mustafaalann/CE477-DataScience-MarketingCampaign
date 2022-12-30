7949d44331b745603b432e43e534262e4a116a0d
mustafaalann <mustafa.alann@hotmail.com>
2022-12-30 23:39:36 +0300

    DBSCAN finished

--- DBSCAN.py
+++ DBSCAN.py
@@ -19,38 +19,21 @@ standardizedData = pd.DataFrame(standardizedData,index=columnsToRescale.index,co
 print('After StandardizationN')
 print(standardizedData)
 X = standardizedData
-
-def IQR(x):  # Q3 - Q1
-    _IQR = np.quantile(dataset[x].dropna(), 0.75) - np.quantile(dataset[x].dropna(), 0.25)
-    return _IQR
-
-def lowerFence(x):  # Q1 - 1.5 * IQR
-    lowerFen = np.quantile(dataset[x].dropna(), 0.25) - 1.5 * IQR(x)
-    return lowerFen
+X.reset_index(drop=True)
 
 
-def upperFence(x):  # Q3 + 1.5 * IQR
-    upperFen = np.quantile(dataset[x].dropna(), 0.75) + 1.5 * IQR(x)
-    return upperFen
 
-print(X['Income'][0] )
-for i in range(len(X)):
-    if(X['Income'][i] < lowerFence("Income") or X['Income'][i] > upperFence("Income")):
-        X.drop(X['Income'][i])
-
-for i in range(len(X)):
-    if(X['MntWines'][i] < lowerFence("MntWines") or X['MntWines'][i] > upperFence("MntWines")):
-        X.drop(X['MntWines'][i])
+from scipy import stats
+X = X[(np.abs(stats.zscore(X)) < 3).all(axis=1)]
 
 
 
 
-
-
-clusterer = DBSCAN(eps=0.3, min_samples=10, metric='euclidean')
+clusterer = DBSCAN(eps=0.3, min_samples=100, metric='euclidean')
 y_pred = clusterer.fit_predict(X)
 
 plt.figure(figsize=(12,9))
-plt.annotate('CE 477', xy=(0.03, 0.95), xycoords='axes fraction')
-plt.scatter(X.iloc[:, 0], X.iloc[:, 1], c=y_pred, s=50, cmap='Dark2')
+plt.annotate('Income and MntWines DBSCAN', xy=(0.03, 0.95), xycoords='axes fraction')
+plt.scatter(X.iloc[:, 0], X.iloc[:, 1], c=y_pred, s=50, cmap='rainbow')
 plt.show()
+print(len(y_pred))
