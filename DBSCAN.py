3d5d4584891b6fee6f1136578d743920be40685e
mustafaalann <mustafa.alann@hotmail.com>
2022-12-30 23:12:55 +0300

    deneme

--- DBSCAN.py
+++ DBSCAN.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn
@@ -19,9 +20,37 @@ print('After Standardization')
 print(standardizedData)
 X = standardizedData
 
+def IQR(x):  # Q3 - Q1
+    _IQR = np.quantile(dataset[x].dropna(), 0.75) - np.quantile(dataset[x].dropna(), 0.25)
+    return _IQR
+
+def lowerFence(x):  # Q1 - 1.5 * IQR
+    lowerFen = np.quantile(dataset[x].dropna(), 0.25) - 1.5 * IQR(x)
+    return lowerFen
+
+
+def upperFence(x):  # Q3 + 1.5 * IQR
+    upperFen = np.quantile(dataset[x].dropna(), 0.75) + 1.5 * IQR(x)
+    return upperFen
+
+print(X['Income'][0] )
+for i in range(len(X)):
+    if(X['Income'][i] < lowerFence("Income") or X['Income'][i] > upperFence("Income")):
+        X.drop(X['Income'][i])
+
+for i in range(len(X)):
+    if(X['MntWines'][i] < lowerFence("MntWines") or X['MntWines'][i] > upperFence("MntWines")):
+        X.drop(X['MntWines'][i])
+
+
+
+
+
+
 clusterer = DBSCAN(eps=0.3, min_samples=10, metric='euclidean')
 y_pred = clusterer.fit_predict(X)
 
 plt.figure(figsize=(12,9))
 plt.annotate('CE 477', xy=(0.03, 0.95), xycoords='axes fraction')
 plt.scatter(X.iloc[:, 0], X.iloc[:, 1], c=y_pred, s=50, cmap='Dark2')
+plt.show()
